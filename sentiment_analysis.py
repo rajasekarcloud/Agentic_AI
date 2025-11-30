1f6582365359d3d03017cf7fa3f13c2edb112426
Rajasekar Munuswamy <rajasekar.unix@yahoo.com>
2025-11-30 09:40:52 -0800

    Change sentiment analysis model to RoBERTa
    Updated sentiment analysis model from DistilBERT to RoBERTa.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,13 +2,22 @@ from transformers import pipeline
 # Hugging Face Transformers, the pipeline will by default load the model:
 # A DistilBERT model fine‑tuned on the Stanford Sentiment Treebank v2 (SST‑2) dataset.
 # - It predicts positive or negative sentiment for English text.
-sentiment = pipeline("sentiment-analysis")
+#sentiment = pipeline("sentiment-analysis")
+# So by default, pipeline("sentiment-analysis")
+# uses DistilBERT fine‑tuned on SST‑2, but you can plug in RoBERTa, BERT, or any other model you prefer.
+sentiment = pipeline("sentiment-analysis", model="roberta-base")
 result1 = sentiment("I love AI!")
 result2 = sentiment("I hate AI!")
 result3 = sentiment("I hate and love AI!")
 print(result1)
 print(result2)
 print(result3)
+# sentiment = pipeline("sentiment-analysis", model="roberta-base")
+[{'label': 'LABEL_1', 'score': 0.5358943343162537}]
+[{'label': 'LABEL_1', 'score': 0.535211443901062}]
+[{'label': 'LABEL_1', 'score': 0.535454511642456}]
+
+#  pipeline("sentiment-analysis")
 [{'label': 'POSITIVE', 'score': 0.9998650550842285}]
 [{'label': 'NEGATIVE', 'score': 0.9992353916168213}]
 [{'label': 'POSITIVE', 'score': 0.9994539618492126}]
